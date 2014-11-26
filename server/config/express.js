2d6563073bc6be8e44262e8f053a7b06c6871a8a
Chris Trude <christrude@gmail.com>
2014-11-25 23:12:42 -0500

    Working on advanced search, WIP

--- server/config/express.js
+++ server/config/express.js
@@ -5,7 +5,7 @@
 'use strict';
 
 var express = require('express');
-var favicon = require('static-favicon');
+var favicon = require('serve-favicon');
 var morgan = require('morgan');
 var compression = require('compression');
 var bodyParser = require('body-parser');
@@ -43,7 +43,7 @@ module.exports = function(app) {
   }));
 
   if ('production' === env) {
-    app.use(favicon(path.join(config.root, 'public', 'favicon.ico')));
+    app.use(favicon(path.join(config.root, 'public', 'favicon.png')));
     app.use(express.static(path.join(config.root, 'public')));
     app.set('appPath', config.root + '/public');
     app.use(morgan('dev'));
