bd838e9a74a602442eee9556c4baf638c624ba1a
Chris Trude <christrude@gmail.com>
2014-12-15 23:59:01 -0500

    WIP listings, agents etc,

--- client/app/agents/agents.directive.js
+++ client/app/agents/agents.directive.js
@@ -5,13 +5,22 @@ angular.module('cruvitaApp')
     return {
       templateUrl: 'app/agents/agents.html',
       restrict: 'EA',
+      scope: {
+        listing: '='
+      },
       link: function (scope, element, attrs) {
       },
       controller: ['$scope', 'User', function($scope, User){
+
         $http.get('/api/users').success(function(users) {
-          console.log(users[0]);
+          console.log('user', users[0]);
           $scope.users = users;
         });
+        console.log('listing', $scope.listing);
+        $scope.activeAgent = _.find($scope.users, function(u){
+          return u.paidInterests.schools === listing
+        })
+
       }]
     };
   });
