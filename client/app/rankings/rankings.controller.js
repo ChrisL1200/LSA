ee263069ffb6a5ff1ea367919e595f7824d7d992
Christopher Lloyd <clloyd1212@gmail.com>
2014-12-22 22:24:44 -0500

    Fixed rankings to use new query API

--- client/app/rankings/rankings.controller.js
+++ client/app/rankings/rankings.controller.js
@@ -5,8 +5,14 @@ angular.module('cruvitaApp')
   	$scope.getLocation = Location.autocomplete;
 
     $scope.getRankings = function() {
-    	var requestObject = Location.getRequest($scope.locationSelected, false);
-    	$scope.schoolPromise = School.retrieve(requestObject, {}, function(response) {
+    	var queries = [];
+    	var selected =  _.where(Location.lastSelected, { 'formatted_address': $scope.locationSelected })[0];
+    	angular.forEach(selected.address_components, function(component) {
+    		if(component.types[0] !== 'country' && component.types[0] !== 'administrative_area_level_2') {
+    			queries.push({key: Location.schoolsComponentMap[component.types[0]], value: component.short_name, type: 'equals'});
+	    	}
+    	})
+    	$scope.schoolPromise = School.retrieve({}, {queries: queries}, function(response) {
         $scope.schools = response;
       }).$promise;
     }
